8be2316109cee329b8920179f0427bfa7bd53f29
theGeniusDennis <danim005@st.ug.edu.gh>
2024-08-23 19:11:43 -0700

    Working on the Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='h-screen bg-black'>
+      <div className="h-[100%] flex">
+      <Sidebar/>
+      </div>
     </div>
   )
 }
