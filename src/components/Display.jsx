55a1a1d86a21a4b7745f78000eae272cf8fc8f4e
theGeniusDennis <danim005@st.ug.edu.gh>
2024-08-25 23:01:16 -0700

    Update

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useRef } from 'react'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 const Display = () => {
+
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album")
+  const albumId =     isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+
   return (
-    <div className='w-[100%] m-2 px-6 rounded bg-[]#121212 text-white overflow-auto lg:w-[75%] lg:ml-0' >
+    <div ref={displayRef} className='w-[100%] m-2 px-6 rounded bg-[]#121212 text-white overflow-auto lg:w-[75%] lg:ml-0' >
        <Routes>
         <Route path="/" element={<DisplayHome/>} />
         <Route path="/Album/:id" element={<DisplayAlbum/>} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+
+
+
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-        <img src={assets.arrow_left} alt="" className='w-8 bg-black p-2 rounded-2xl cursor-pointer' />
-        <img src={assets.arrow_right} alt="" className='w-8 bg-black p-2 rounded-2xl cursor-pointer' />
+        <img onClick={() => navigate(-1)} src={assets.arrow_left} alt="" className='w-8 bg-black p-2 rounded-2xl cursor-pointer' />
+        <img onClick={() => navigate(1)} src={assets.arrow_right} alt="" className='w-8 bg-black p-2 rounded-2xl cursor-pointer' />
         </div>
         <div className="flex items-center gap-4">
           <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block
